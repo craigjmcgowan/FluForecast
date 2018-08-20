8706fff962d3fb4032ab7a3657569724a822511e
Craig McGowan <mcgowan.cj@gmail.com>
2018-08-20 20:26:08 +0200

    Restructure historical forecasts folder

--- R/calculate_scores.R
+++ R/calculate_scores.R
@@ -1,3 +1,10 @@
 # Generate scores
 library(tidyverse)
 library(FluSight)
+
+source("R/read_forecasts.R")
+# Read in retrospective forecasts -------
+subtype_forecasts <- read_forecasts("Forecasts/Subtype Historical Average")
+verify_entry_file("Forecasts/Subtype Historical Average/2012-2013/EW1.csv")
+
+dir <- "Forecasts/Subtype Historical Average"
