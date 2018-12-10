59cceb98b5bab568719763c41d8fe80d80858215
Craig McGowan <mcgowan.cj@gmail.com>
2018-12-10 06:38:44 +0200

    Week 48 forecasts

--- R/create_prospective_forecast.R
+++ R/create_prospective_forecast.R
@@ -13,8 +13,8 @@ source("R/utils.R")
 source("R/create_subtype_forecast.R")
 
 ##### Set week that forecasts are being based on #####
-EW <- 47
-epiweek <- 201847
+EW <- 48
+epiweek <- 201848
 
 ##### Update data #####
 source("R/read_data.R")
