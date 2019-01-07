be1d192fd5da6a89934bbe098daf92e0f1040bdc
Craig McGowan <mcgowan.cj@gmail.com>
2019-01-07 14:03:37 +0200

    Week 52 forecasts

--- R/create_prospective_forecast.R
+++ R/create_prospective_forecast.R
@@ -13,8 +13,8 @@ source("R/utils.R")
 source("R/create_subtype_forecast.R")
 
 ##### Set week that forecasts are being based on #####
-EW <- 51
-epiweek <- 201851
+EW <- 52
+epiweek <- 201852
 
 ##### Update data #####
 source("R/read_data.R")
