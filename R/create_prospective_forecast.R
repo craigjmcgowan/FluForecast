1635453e6814462955785262725e8c5e3b62ce73
Craig McGowan <mcgowan.cj@gmail.com>
2019-02-17 13:25:10 +0200

    Week 6 forecasts

--- R/create_prospective_forecast.R
+++ R/create_prospective_forecast.R
@@ -13,8 +13,8 @@ source("R/utils.R")
 source("R/create_subtype_forecast.R")
 
 ##### Set week that forecasts are being based on #####
-EW <- 05
-epiweek <- 201905
+EW <- 06
+epiweek <- 201906
 
 ##### Update data #####
 source("R/read_data.R")
