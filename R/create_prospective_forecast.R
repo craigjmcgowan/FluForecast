a5914368696d5dead4c8dc94d4dc9a5c5b80469f
Craig McGowan <mcgowan.cj@gmail.com>
2019-04-01 07:48:40 -0400

    Week 12 Forecasts

--- R/create_prospective_forecast.R
+++ R/create_prospective_forecast.R
@@ -13,8 +13,8 @@ source("R/utils.R")
 source("R/create_subtype_forecast.R")
 
 ##### Set week that forecasts are being based on #####
-EW <- 11
-epiweek <- 201911
+EW <- 12
+epiweek <- 201912
 
 ##### Update data #####
 source("R/read_data.R")
