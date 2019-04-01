8988067791187f2515c532b3ac779d4883dcc428
Craig McGowan <mcgowan.cj@gmail.com>
2019-04-01 15:39:21 -0400

    Update gtrends_pull_data to pull Google Trends data for all states

--- R/create_prospective_forecast.R
+++ R/create_prospective_forecast.R
@@ -13,8 +13,8 @@ source("R/utils.R")
 source("R/create_subtype_forecast.R")
 
 ##### Set week that forecasts are being based on #####
-EW <- 05
-epiweek <- 201905
+EW <- 12
+epiweek <- 201912
 
 ##### Update data #####
 source("R/read_data.R")
