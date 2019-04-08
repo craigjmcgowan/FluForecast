8817096b3bfede9af2bb82c22684270a252be05d
Craig McGowan <mcgowan.cj@gmail.com>
2019-04-08 07:57:54 -0400

    Week 13 Forecasts

--- R/create_prospective_forecast.R
+++ R/create_prospective_forecast.R
@@ -13,8 +13,8 @@ source("R/utils.R")
 source("R/create_subtype_forecast.R")
 
 ##### Set week that forecasts are being based on #####
-EW <- 12
-epiweek <- 201912
+EW <- 13
+epiweek <- 201913
 
 ##### Update data #####
 source("R/read_data.R")
