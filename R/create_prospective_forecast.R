bd814854d35b5a0ff76e9503ffe71efd9979dd4f
Craig McGowan <mcgowan.cj@gmail.com>
2019-04-22 10:57:59 -0400

    Week 15 Forecasts

--- R/create_prospective_forecast.R
+++ R/create_prospective_forecast.R
@@ -13,8 +13,8 @@ source("R/utils.R")
 source("R/create_subtype_forecast.R")
 
 ##### Set week that forecasts are being based on #####
-EW <- 14
-epiweek <- 201914
+EW <- 15
+epiweek <- 201915
 
 ##### Update data #####
 source("R/read_data.R")
