4f1d01c9d462a15ceea0df7076a9fbe39eb4e65d
Craig McGowan <mcgowan.cj@gmail.com>
2019-11-01 19:21:59 -0400

    Add state backfill densities

--- R/create_backfill_density.R
+++ R/create_backfill_density.R
@@ -18,16 +18,18 @@ fit_density <- function(x) {
 # Load data and  create densities for each location, week, lag combination
 ili_current <- readRDS('Data/ili_current.RDS') %>%
   mutate(release_date = as.Date(release_date)) %>%
-  filter(!location %in% state.name, season != "2019/2020")
+  filter(season != "2019/2020")
 
 ili_scored <- readRDS('Data/ili_init_pub_list.RDS')[c('201428', '201528', '201628', 
                                                       '201728', '201828', '201928')] %>%
   bind_rows() %>%
-  filter(season != "2012/2013", !location %in% state.name) %>%
+  filter(season != "2012/2013") %>%
   bind_rows(filter(ili_current, 
-                   season %in% c('2010/2011', '2011/2012', '2012/2013'))) 
+                   season %in% c('2010/2011', '2011/2012', '2012/2013'),
+                   !location %in% state.name))
 
-ili_backfill <- readRDS('Data/ili_init_pub_list.RDS') %>%
+#####  Calculate backfill densities for national/regional #####
+ili_backfill_nat_reg <- readRDS('Data/ili_init_pub_list.RDS') %>%
   lapply(function(x) dplyr::filter(x, !location %in% state.name)) %>%
   lapply(add_scored_value) %>%
   bind_rows() %>%
@@ -38,25 +40,25 @@ ili_backfill <- readRDS('Data/ili_init_pub_list.RDS') %>%
   select(location, print_year, season, week, measure_week, backfill)
 
 
-ili_backfill_densities <- ili_backfill %>%
+ili_backfill_densities_nat_reg <- ili_backfill_nat_reg %>%
   # Add in rows for 2019/2020
-  bind_rows(filter(ili_backfill, season == '2018/2019') %>%
+  bind_rows(filter(ili_backfill_nat_reg, season == '2018/2019') %>%
               mutate(print_year = print_year + 1,
                      season = '2019/2020',
                      backfill = NA_real_)) %>%
   select(location, print_year, season, week, measure_week) %>%
   mutate(full_data = pmap(list(location, week, measure_week),
-                          ~ filter(ili_backfill, location == ..1,
+                          ~ filter(ili_backfill_nat_reg, location == ..1,
                                    week == ..2, measure_week == ..3) %>%
                             select(backfill)),
          sub_data = pmap(list(location, print_year, week, measure_week),
-                         ~ filter(ili_backfill, location == ..1,
+                         ~ filter(ili_backfill_nat_reg, location == ..1,
                                   print_year < max(..2, 2015), 
                                   week == ..3, measure_week == ..4) %>%
                            select(backfill)),
          data = ifelse(location != "US National" & week %in% c(40, 73),
                        full_data, sub_data),
-         density_bw = map(data,
+         density_bw = map_dbl(data,
                           ~ fit_density(.$backfill))) %>%
   select(location, season, week, measure_week, data, density_bw) 
 
@@ -68,11 +70,69 @@ ili_fill_densities <- crossing(location = c("HHS Region 1", "HHS Region 2", "HHS
                            season = c("2010/2011", "2011/2012", "2012/2013", "2013/2014"),
                            week = 40,
                            measure_week = 40) %>%
-  left_join(filter(ili_backfill_densities, season == "2014/2015") %>%
+  left_join(filter(ili_backfill_densities_nat_reg, season == "2014/2015") %>%
                select(-season),
             by = c('location', 'week', 'measure_week'))
 
+##### Calculate state backfill densities #####
+ili_backfill_state <- readRDS('Data/ili_init_pub_list.RDS') %>%
+  lapply(function(x) dplyr::filter(x, location %in% state.name)) %>%
+  lapply(add_scored_value) %>%
+  bind_rows() %>%
+  mutate(measure_week = week_inorder(week, season),
+         week = measure_week + lag,
+         print_year = as.numeric(substr(season, 1, 4))) %>%
+  filter(lag <= 35, measure_week <= 73, week <= 73) %>%
+  select(location, print_year, season, week, measure_week, backfill)
+
+
+# Identify locations with no backfill either year or one observation
+ili_backfill_no_backfill <- ili_backfill_state %>%
+  group_by(location, week, measure_week) %>%
+  filter(near(min(backfill), max(backfill))) %>%
+  select(location, week, measure_week) %>%
+  distinct() %>%
+  mutate(no_dens = 1)
+
+
+ili_backfill_data_state <- ili_backfill_state %>%
+  # Identify locations with one obs, no backfill, or identical values either year
+  left_join(ili_backfill_no_backfill,
+            by = c('location', 'week', 'measure_week')) %>%
+  # Keep single location/week/lag combo - same estimate for all years
+  select(location, week, measure_week, no_dens) %>%
+  distinct() %>%
+  mutate(data = pmap(list(location, week, measure_week),
+                          ~ filter(ili_backfill_state, location == ..1,
+                                   week == ..2, measure_week == ..3) %>%
+                            select(backfill)))
+
+ili_backfill_densities_state <- ili_backfill_data_state %>%
+  filter(is.na(no_dens)) %>%
+  mutate(density_bw = map_dbl(data, ~ fit_density(.$backfill))) %>%
+  select(location, week, measure_week, density_bw) 
+
+# Add BW equal to median for that location in for locations with no prior backfill or insufficient data
+med_bw_by_state <- ili_backfill_densities_state %>%
+  group_by(location) %>%
+  summarize(med_bw = median(density_bw))
+
+
+ili_backfill_densities_state_full <- ili_backfill_data_state %>%
+  select(-no_dens) %>%
+  left_join(ili_backfill_densities_state, 
+            by = c('location', 'week', 'measure_week')) %>%
+  left_join(med_bw_by_state, by = 'location') %>%
+  mutate(density_bw = case_when(!is.na(density_bw) ~ density_bw,
+                                !is.na(med_bw) ~ med_bw,
+                                TRUE ~ median(ili_backfill_densities_state$density_bw)))  %>%
+  select(-med_bw)
+
 # Save densities
-bind_rows(ili_backfill_densities, ili_fill_densities) %>%
+bind_rows(ili_backfill_densities_nat_reg, ili_fill_densities, 
+          ili_backfill_densities_state_full) %>%
   nest(backfill = c(measure_week, data, density_bw)) %>%
   saveRDS("Data/ili_backfill_densities.Rds")
+
+
+
