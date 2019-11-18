a7b1c9c0d3c1edef703d7d7bb8dcc815a0ef1739
Craig McGowan <mcgowan.cj@gmail.com>
2019-11-18 07:24:51 -0500

    Week 45 Forecasts

--- R/create_prospective_forecast.R
+++ R/create_prospective_forecast.R
@@ -13,8 +13,8 @@ source("R/utils.R")
 source("R/create_subtype_forecast.R")
 
 ##### Set week that forecasts are being based on #####
-EW <- 44
-epiweek <- 201944
+EW <- 45
+epiweek <- 201945
 EW_paste <- str_pad(EW, 2, pad = "0")
 order_week <- ifelse(EW < 40, EW + 52, EW)
 
